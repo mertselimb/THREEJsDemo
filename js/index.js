192d6b0f9e79aaf4c977a349c9c6b18ee03a43b6
Mert Selimbeyoğlu <mertselimb@gmail.com>
2019-08-20 11:55:07 +0300

    ver1

--- js/index.js
+++ js/index.js
@@ -1,62 +1,84 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var materialRed = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-var materialGreen = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var materialBlue = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var materialRed = new THREE.MeshLambertMaterial({ color: 0xcccc00 });
+var materialGreen = new THREE.MeshLambertMaterial({ color: 0x202020 });
+var materialBlue = new THREE.MeshLambertMaterial({ color: 0xffffff });
 
-arrX = [-4,-3,-2,-1,0,1,2,3,4];
-arrY = [-4,-3,-2,-1,0,1,2,3,4];
+len = 15;
+arrCoords = [];
+
+for (let i = 0; i < len; i = i + 2) {
+  arrCoords.push(i);
+  arrCoords.push(-i);
+}
+
 cubes = [];
 
-arrX.forEach(x => {
-    arrY.forEach(y => {
-        var r = new THREE.Mesh( geometry, materialRed );
-        var g = new THREE.Mesh( geometry, materialGreen );
-        var b = new THREE.Mesh( geometry, materialBlue );
+arrCoords.forEach(x => {
+  arrCoords.forEach(y => {
+    var r = new THREE.Mesh(geometry, materialRed);
+    var g = new THREE.Mesh(geometry, materialGreen);
+    var b = new THREE.Mesh(geometry, materialBlue);
 
-        scene.add(r);
-        scene.add(g);
-        scene.add(b);
+    scene.add(r);
+    scene.add(g);
+    scene.add(b);
 
-        r.position.y = y;
-        g.position.y = y;
-        b.position.y = y;
-        r.position.x = x;
-        g.position.x = x;
-        b.position.x = x;
+    r.position.y = y;
+    g.position.y = y;
+    b.position.y = y;
+    r.position.x = x;
+    g.position.x = x;
+    b.position.x = x;
 
-        cubes.push({r,g,b});
-    })
-})
+    cubes.push({ r, g, b });
+  });
+});
 
-camera.position.z = 5;
+camera.position.z = 15;
+mul = 1;
+rotation = 0.01;
 
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+scene.add(directionalLight);
+directionalLight.position.x = 25;
+directionalLight.position.y = 5;
+directionalLight.position.z = 15;
 
 var update = _ => {
-    var index = 0
-    cubes.forEach(cube => {
-        cube.r.rotation.x += 0.02;
-        cube.g.rotation.y += 0.02;
-        cube.b.rotation.z += 0.02;
-    })
+  cubes.forEach(cube => {
+    cube.r.rotation.x += rotation;
+    cube.g.rotation.y += rotation;
+    cube.b.rotation.z += rotation;
+  });
+//   camera.position.z += 0.1 * mul;
+//   camera.rotation.z += 0.01;
+//   if (camera.position.z > 20 || camera.position.z < 0) {
+//     mul = mul * -1;
+//   }
 
-}
+};
 
 var render = _ => {
-    renderer.render( scene, camera);
-}
+  renderer.render(scene, camera);
+};
 
 var GameLoop = _ => {
-    requestAnimationFrame(GameLoop);
+  requestAnimationFrame(GameLoop);
 
-    update();
-    render();
-}
+  update();
+  render();
+};
 
 GameLoop();
